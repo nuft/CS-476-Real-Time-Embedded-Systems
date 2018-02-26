259778932515d5775dea62c5441baeb2c7e1cbf9
Michael Spieler <michael.spieler@epfl.ch>
2018-02-26 12:17:32 +0100

    port map WIP

--- Lab1-InterruptAnalysis/hw/hdl/DE1_SoC_top_level.vhd
+++ Lab1-InterruptAnalysis/hw/hdl/DE1_SoC_top_level.vhd
@@ -164,6 +164,47 @@ end entity DE1_SoC_top_level;
 
 architecture rtl of DE1_SoC_top_level is
 
-begin
+component soc_system is
+    port (
+        clk_clk                                   : in    std_logic                     := 'X';             -- clk
+        reset_reset_n                             : in    std_logic                     := 'X';             -- reset_n
+        sdram_controller_0_wire_addr              : out   std_logic_vector(12 downto 0);                    -- addr
+        sdram_controller_0_wire_ba                : out   std_logic_vector(1 downto 0);                     -- ba
+        sdram_controller_0_wire_cas_n             : out   std_logic;                                        -- cas_n
+        sdram_controller_0_wire_cke               : out   std_logic;                                        -- cke
+        sdram_controller_0_wire_cs_n              : out   std_logic;                                        -- cs_n
+        sdram_controller_0_wire_dq                : inout std_logic_vector(15 downto 0) := (others => 'X'); -- dq
+        sdram_controller_0_wire_dqm               : out   std_logic_vector(1 downto 0);                     -- dqm
+        sdram_controller_0_wire_ras_n             : out   std_logic;                                        -- ras_n
+        sdram_controller_0_wire_we_n              : out   std_logic;                                        -- we_n
+        parallel_port_0_external_interface_export : in    std_logic_vector(3 downto 0)  := (others => 'X')  -- export
+    );
+end component soc_system;
 
+begin
+u0 : component soc_system
+    port map (
+        clk_clk                                   => CLOCK_50,                                   --                                clk.clk
+        reset_reset_n                             => KEY_N(0),                             --                              reset.reset_n
+        sdram_controller_0_wire_addr              => CONNECTED_TO_sdram_controller_0_wire_addr,              --            sdram_controller_0_wire.addr
+        sdram_controller_0_wire_ba                => CONNECTED_TO_sdram_controller_0_wire_ba,                --                                   .ba
+        sdram_controller_0_wire_cas_n             => CONNECTED_TO_sdram_controller_0_wire_cas_n,             --                                   .cas_n
+        sdram_controller_0_wire_cke               => CONNECTED_TO_sdram_controller_0_wire_cke,               --                                   .cke
+        sdram_controller_0_wire_cs_n              => CONNECTED_TO_sdram_controller_0_wire_cs_n,              --                                   .cs_n
+        sdram_controller_0_wire_dq                => CONNECTED_TO_sdram_controller_0_wire_dq,                --                                   .dq
+        sdram_controller_0_wire_dqm               => CONNECTED_TO_sdram_controller_0_wire_dqm,               --                                   .dqm
+        sdram_controller_0_wire_ras_n             => CONNECTED_TO_sdram_controller_0_wire_ras_n,             --                                   .ras_n
+        sdram_controller_0_wire_we_n              => CONNECTED_TO_sdram_controller_0_wire_we_n,              --                                   .we_n
+        parallel_port_0_external_interface_export => KEY_N  -- parallel_port_0_external_interface.export
+    );
 end;
+
+--sdram_controller_0_wire_addr => DRAM_ADDR,
+--sdram_controller_0_wire_ba => DRAM_BA,
+--sdram_controller_0_wire_cas_n => DRAM_CAS_N,
+--sdram_controller_0_wire_cke => DRAM_CKE,
+--sdram_controller_0_wire_cs_n => DRAM_CS_N,
+--sdram_controller_0_wire_dq => DRAM_DQ,
+--sdram_controller_0_wire_dqm(1) => DRAM_UDQM,
+--sdram_controller_0_wire_dqm(0) => DRAM_LDQM,
+--sdram_controller_0_wire_ras_n => DRAM_RAS_N,
